f3b36684ee22941aa4344b1f7c5dec36f5c7f1ca
Bob Forcha <bobforcha@gmail.com>
2018-12-31 00:36:21 -0500

    Implement user signup

---
+++ spec/features/users/signing_users_up_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "User signup", js: true do
+  scenario "with valid credentials" do
+    visit root_path
+    click_link "Sign up"
+
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("You have signed up successfully.")
+  end
+end
