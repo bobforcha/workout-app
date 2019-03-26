f526059bc3858800598e02de536fca90b5d34359
Bob Forcha <bobforcha@gmail.com>
2019-03-25 23:30:00 -0400

    Ensure only last 7 days are shown

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,4 +4,6 @@ class Exercise < ApplicationRecord
   validates :duration, numericality: { greater_than: 0.0 }
   validates :details, presence: true
   validates :activity_date, presence: true
+
+  default_scope { where('activity_date > ?', 7.days.ago).order(activity_date: :desc) }
 end
--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -7,9 +7,10 @@ RSpec.feature 'Listing Exercises', js: true do
     login_as john
   end
 
-  context 'when exercises are created' do
+  fcontext 'when exercises are created' do
     let!(:e1)   { john.exercises.create(duration: 20, details: "My body building activity", activity_date: 2.days.ago) }
     let!(:e2)   { john.exercises.create(duration: 55, details: "Weight lifting", activity_date: 2.days.ago) }
+    let!(:e3)   { john.exercises.create(duration: 30, details: "Squats", activity_date: 8.days.ago) }
 
     it "shows user's workout for last 7 days" do
       visit root_path
@@ -22,10 +23,14 @@ RSpec.feature 'Listing Exercises', js: true do
       expect(page).to have_content(e2.duration)
       expect(page).to have_content(e2.details)
       expect(page).to have_content(e2.activity_date)
+
+      expect(page).not_to have_content(e3.duration)
+      expect(page).not_to have_content(e3.details)
+      expect(page).not_to have_content(e3.activity_date)
     end
   end
 
-  context 'when no exercises are created' do
+  fcontext 'when no exercises are created' do
     it "shows no workouts" do
       visit root_path
       click_link 'My Lounge'
