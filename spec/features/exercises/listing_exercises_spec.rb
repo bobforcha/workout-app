85b4334a58f0e7fa429a145816eada866d7337f1
Bob Forcha <bobforcha@gmail.com>
2019-03-31 23:27:49 -0400

    Implement editing exercises

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -8,25 +8,27 @@ RSpec.feature 'Listing Exercises', js: true do
   end
 
   context 'when exercises are created' do
-    let!(:e1)   { john.exercises.create(duration: 20, details: "My body building activity", activity_date: 2.days.ago) }
-    let!(:e2)   { john.exercises.create(duration: 55, details: "Weight lifting", activity_date: 2.days.ago) }
+    let!(:e1)   { john.exercises.create(duration: 20, details: "My body building activity", activity_date: 4.days.ago) }
+    let!(:e2)   { john.exercises.create(duration: 55, details: "Weight lifting", activity_date: 4.days.ago) }
     let!(:e3)   { john.exercises.create(duration: 30, details: "Squats", activity_date: 8.days.ago) }
 
     it "shows user's workout for last 7 days" do
       visit root_path
       click_link 'My Lounge'
-  
-      expect(page).to have_content(e1.duration)
-      expect(page).to have_content(e1.details)
-      expect(page).to have_content(e1.activity_date)
-  
-      expect(page).to have_content(e2.duration)
-      expect(page).to have_content(e2.details)
-      expect(page).to have_content(e2.activity_date)
 
-      expect(page).not_to have_content(e3.duration)
-      expect(page).not_to have_content(e3.details)
-      expect(page).not_to have_content(e3.activity_date)
+      exercise_table = find('.table')
+  
+      expect(exercise_table).to have_content(e1.duration)
+      expect(exercise_table).to have_content(e1.details)
+      expect(exercise_table).to have_content(e1.activity_date)
+  
+      expect(exercise_table).to have_content(e2.duration)
+      expect(exercise_table).to have_content(e2.details)
+      expect(exercise_table).to have_content(e2.activity_date)
+
+      expect(exercise_table).not_to have_content(e3.duration)
+      expect(exercise_table).not_to have_content(e3.details)
+      expect(exercise_table).not_to have_content(e3.activity_date)
     end
   end
 
