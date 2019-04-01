3199b13c691d767a14edee896bcd611247cc91fc
Bob Forcha <bobforcha@gmail.com>
2019-04-01 00:42:05 -0400

    Add validations to user

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "User signup", js: true do
-  scenario "with valid credentials" do
+  before do
     visit root_path
-    click_link "Sign up"
+    click_link 'Sign up'
+  end
 
+  fscenario "with valid credentials" do
     fill_in 'First name', with: 'John'
     fill_in 'Last name', with: 'Doe'
     fill_in "Email", with: "john@example.com"
@@ -17,4 +19,18 @@ RSpec.feature "User signup", js: true do
     visit root_path
     expect(page).to have_content('John Doe')
   end
+
+  fscenario "with invalid credentials" do
+    fill_in 'First name', with: ''
+    fill_in 'Last name', with: ''
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+  end
+
+
 end
