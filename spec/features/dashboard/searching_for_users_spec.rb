949cf8982a8c83c232f9b23c3eff32243ce21481
Bob Forcha <bobforcha@gmail.com>
2019-04-13 11:42:33 -0400

    Write following friends spec

--- spec/features/dashboard/searching_for_users_spec.rb
+++ spec/features/dashboard/searching_for_users_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Searching for users', js: true do
   let!(:john)  { User.create(first_name: 'John', last_name: 'Doe', email: 'john@example.com', password: 'password') }
   let!(:sarah)  { User.create(first_name: 'Sarah', last_name: 'Doe', email: 'sarah@example.com', password: 'password') }
 
-  fcontext "with existing name" do
+  context "with existing name" do
     it 'returns all those users' do
       visit root_path
       fill_in 'search_name', with: 'Doe'
---
+++ spec/features/following/following_friends_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.feature "Following Friends", js: true do
+  let!(:john)  { User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password") }
+  let!(:peter)  { User.create(first_name: "Peter", last_name: "Corn", email: "peter@example.com", password: "password") }
+
+  context "when signed in" do
+    before do
+      login_as john
+      visit "/"
+    end
+
+    describe "the lounge page" do
+      it "displays both users' names" do
+        expect(page).to have_content(john.full_name)
+        expect(page).to have_content(peter.full_name)
+      end
+
+      it "does not allow following oneself" do
+        self_link = "/friendships?friend_id=#{john.id}"
+
+        expect(page).not_to have_link("Follow", href: self_link)
+      end
+
+      it "has a follow link for other users" do
+        other_link = "/friendships?friend_id=#{peter.id}"
+        expect(page).to have_link("Follow", href: other_link)
+      end
+    end
+
+    context "after following someone" do
+      before do
+        follow_link = "a[href='/friendships?friend_id=#{peter.id}']"
+        find(follow_link).click
+      end
+
+      it "no longer shows that person's follow link" do
+        expect(page).not_to have_link("Follow", href: follow_link)
+      end
+    end
+  end
+end
